05654ca0c7d91d1ef55b96eb4dbe9e94f251312f
3LOG1 <syamkrishnants@gmail.com>
2024-03-19 12:53:15 +0530

    new nebula1

---
+++ nebula1.py
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Prompt for password
+echo "Please Enter the listsearch password"
+
+sleep 5
+read -s -p "Password:"  password
+echo
+
+#read $password
+
+echo $password >>  /opt/password.txt
+
+sleep 3
+
+
+echo "=================="
+echo "Password Updated"
+echo "=================="
+
+# Check if password is correct
+#if [[ "$password" == "" ]]; then
+#    echo "Access granted."
+#else
+#    echo "Access denied."
+#fi
+
